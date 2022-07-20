0d00be9da3c66126197c233fe384c3b520c2fbd5
wachid <wachidnurafif29@gmail.com>
2022-07-20 22:12:42 +0700

    [FIX] ready to deploy

--- db.js
+++ db.js
@@ -1,15 +1,25 @@
-const postgre = require("pg").Pool;
-const { Pool } = require("pg/lib");
+const { Pool, Client } = require("pg");
 require("dotenv").config();
 //CREATE ROLE wachid29 WITH LOGIN PASSWORD 'pasword';
+let connection;
+console.log(process.env.ENV_MODE);
 
-const connection = new Pool({
-  user: process.env.USER,
-  host: process.env.HOST,
-  database: process.env.DATABASE,
-  password: process.env.PASSWORD,
-  port: process.env.PORT2,
-});
+if (process.env.ENV_MODE === "prod") {
+  connection = new Client({
+    connectionString: process.env.DB_URI,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+} else {
+  const connection = new Pool({
+    user: process.env.USER,
+    host: process.env.HOST,
+    database: process.env.DATABASE,
+    password: process.env.PASSWORD,
+    port: process.env.PORT2,
+  });
+}
 
 // //export module biar bisa digunakan ditempat lain
 module.exports = connection;
