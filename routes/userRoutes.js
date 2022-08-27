1a5ef97f9c8c8314891d73fc1243a13604d4f574
Ilham Yusuf <ilham.0992@students.amikom.ac.id>
2022-08-27 22:00:29 +0700

    [ADD] User detail

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,15 +5,11 @@ const middleware = require("../middleware/auth");
 
 Router.get("/user", controller.getUsers);
 Router.post("/user/add", controller.addUsers);
-Router.patch(
-  "/user/editPhoto",
-  middleware.checkToken,
-  upload.uploadprofile,
-  controller.editPhoto
-);
+Router.patch("/user/editPhoto", middleware.checkToken, upload.uploadprofile, controller.editPhoto);
 Router.get("/user/findByID", controller.findUserByID);
 Router.get("/user/findByEmail", controller.findUserByEmail);
 Router.delete("/user/delete", middleware.checkToken, controller.deleteUser);
 Router.patch("/user/edit", middleware.checkToken, controller.editUser);
+Router.patch("/user/edit-detail", middleware.checkToken, controller.editDetailUser);
 
 module.exports = Router;
